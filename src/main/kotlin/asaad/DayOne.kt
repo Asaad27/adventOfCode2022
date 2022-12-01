6a5991ffde8e3e54d89575952968a2309e670601
belarbi <asaad.belarbi@hpe.com>
2022-12-01 20:52:37 +0100

    added Input for DayOne

--- src/main/kotlin/asaad/DayOne.kt
+++ src/main/kotlin/asaad/DayOne.kt
@@ -7,30 +7,28 @@ import java.util.*
 fun main() {
     val filePath = "sampleDayOne/input-day1.txt"
 
-    println("part 1: ")
-    solve(1, filePath)
+    println("part 1: ${solve(1, filePath)}")
+    println("part 2: ${solve(3, filePath)}")
 
-    println("part 2:")
-    solve(3, filePath)
 }
 
 
-fun solve(limit: Int, filePath: String) {
+fun solve(limit: Int, filePath: String): Int{
 
     val file = File(filePath)
-    val priorityQueue = PriorityQueue<Int>(Comparator.comparingInt{it})
+    val priorityQueue = PriorityQueue<Int>(Comparator.comparingInt { it })
 
     file.readText()
         .split("\n\n")
-        .map{ group -> group.lines().sumOf { it.toIntOrNull()?:0 }}
-        .forEach {priorityQueue.addLimited(it, limit)}
+        .map { group -> group.lines().sumOf { it.toIntOrNull() ?: 0 } }
+        .forEach { priorityQueue.addLimited(it, limit) }
 
     var sum = 0
-    repeat(limit){
+    repeat(limit) {
         priorityQueue.apply { sum += this.poll() }
     }
 
-    println(sum)
+    return sum
 }
 
 fun <E> PriorityQueue<E>.addLimited(element: E, limit: Int) {
