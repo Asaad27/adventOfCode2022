73928667535e1862b2c3a0f4f6a76ebc8239ee4b
belarbi <asaad.belarbi@hpe.com>
2022-12-01 20:34:29 +0100

    added Day 1

---
+++ src/main/kotlin/asaad/DayOne.kt
@@ -0,0 +1,41 @@
+package asaad
+
+import java.io.File
+import java.util.*
+
+
+fun main() {
+    val filePath = "sampleDayOne/input-day1.txt"
+
+    println("part 1: ")
+    solve(1, filePath)
+
+    println("part 2:")
+    solve(3, filePath)
+}
+
+
+fun solve(limit: Int, filePath: String) {
+
+    val file = File(filePath)
+    val priorityQueue = PriorityQueue<Int>(Comparator.comparingInt{it})
+
+    file.readText()
+        .split("\n\n")
+        .map{ group -> group.lines().sumOf { it.toIntOrNull()?:0 }}
+        .forEach {priorityQueue.addLimited(it, limit)}
+
+    var sum = 0
+    repeat(limit){
+        priorityQueue.apply { sum += this.poll() }
+    }
+
+    println(sum)
+}
+
+fun <E> PriorityQueue<E>.addLimited(element: E, limit: Int) {
+    add(element)
+    if (size > limit) poll()
+}
+
+
