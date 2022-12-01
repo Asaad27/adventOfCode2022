6eabd914578056c42773bb990ac195947cb7554d
belarbi <asaad.belarbi@hpe.com>
2022-12-01 21:08:15 +0100

    added DayOne

--- src/main/kotlin/asaad/DayOne.kt
+++ src/main/kotlin/asaad/DayOne.kt
@@ -11,10 +11,12 @@ fun main() {
     println("part 1: ${solve(1, input)}")
     println("part 2: ${solve(3, input)}")
 }
+
 fun readInput(file: File) = file.readText()
     .split("\n\n", "\r\n\r\n")
-    .map { group -> group.lines().sumOf { it.toIntOrNull()?:0 }}
-fun solve(limit: Int, elseCalories: List<Int>): Int{
+    .map { group -> group.lines().sumOf { it.toIntOrNull() ?: 0 } }
+
+fun solve(limit: Int, elseCalories: List<Int>): Int {
     val priorityQueue = PriorityQueue<Int>(Comparator.comparingInt { it })
 
     elseCalories.forEach { priorityQueue.addLimited(it, limit) }
